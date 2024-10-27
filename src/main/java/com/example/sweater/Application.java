9f72e06b14ab76dbfd0a598049125dda811b4c58
DemArtem <demar.demidov@yandex.ru>
2024-10-27 13:01:44 +0300

    test

--- src/main/java/com/example/sweater/Application.java
+++ src/main/java/com/example/sweater/Application.java
@@ -1,13 +1,11 @@
-package org.example.sweater;
+package com.example.sweater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
